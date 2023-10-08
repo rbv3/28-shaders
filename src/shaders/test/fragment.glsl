0440ff98a9882bee6de138faf78c06938450057c
Rodrigo Valenca <rvalenca@amazon.com>
2023-10-08 18:20:00 -0300

    change shape and color based on a random float32 array

--- src/shaders/test/fragment.glsl
+++ src/shaders/test/fragment.glsl
@@ -1,5 +1,10 @@
-precision mediump float;
+precision mediump float; // how precise is the float (use mediump as default)
+
+// Retrieve info from VertexShader
+varying float vRandom;
 
 void main() {
-    gl_FragColor = vec4(1.0, 0.0, 0.0, 1.0);
+    // gl_FragColor is the color of the particular pixel
+    // 4th value is the alpha, a, on rgba
+    gl_FragColor = vec4(vRandom, vRandom * 0.5, 1.0, 1.0);
 }
--- src/shaders/test/vertex.glsl
+++ src/shaders/test/vertex.glsl
@@ -4,6 +4,10 @@ uniform mat4 modelMatrix; // transformations relative to the Mesh
 
 // retrieving value from our THREE geometry
 attribute vec3 position;
+attribute float aRandom;
+
+// info to be sent to the fragmentShader
+varying float vRandom;
 
 void main() {
     // gl_Position contains the position of the Vertex on the screen
@@ -13,9 +17,12 @@ void main() {
     // gl_Position = projectionMatrix * viewMatrix * modelMatrix * vec4(position, 1.0);
 
     vec4 modelPosition = modelMatrix * vec4(position, 1.0);
-    modelPosition.z += sin(modelPosition.x * 10.0) * 0.1;
+    // modelPosition.z += sin(modelPosition.x * 10.0) * 0.1;
+    modelPosition.z += aRandom * 0.1;
     
     vec4 viewPosition = viewMatrix * modelPosition;
     vec4 projectedPosition = projectionMatrix * viewPosition;
     gl_Position = projectedPosition;
+
+    vRandom = aRandom;
 }
