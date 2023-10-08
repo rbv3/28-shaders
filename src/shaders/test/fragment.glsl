d62aa93c8901e9f206d15e77616ddb1c25ad855a
Rodrigo Valenca <rvalenca@amazon.com>
2023-10-08 18:58:13 -0300

    add depth to textureColor

--- src/shaders/test/fragment.glsl
+++ src/shaders/test/fragment.glsl
@@ -1,13 +1,16 @@
 precision mediump float; // how precise is the float (use mediump as default)
 
-// Retrieve info from VertexShader
-varying vec2 vUv;
-
 uniform vec3 uColor; // custom uniform
 uniform sampler2D uTexture; // custom uniform
 
+// Retrieve info from VertexShader
+varying vec2 vUv;
+varying float vElevation;
+
 void main() {
     vec4 textureColor = texture2D(uTexture, vUv);
+    textureColor.rgb *= vElevation * 2.0 + 0.7;
+
     // gl_FragColor is the color of the particular pixel
     // 4th value is the alpha, a, on rgba
     gl_FragColor = textureColor;
--- src/shaders/test/vertex.glsl
+++ src/shaders/test/vertex.glsl
@@ -9,6 +9,7 @@ attribute vec3 position;
 attribute vec2 uv;
 
 varying vec2 vUv;
+varying float vElevation;
 
 void main() {
     // gl_Position contains the position of the Vertex on the screen
@@ -18,13 +19,16 @@ void main() {
     // gl_Position = projectionMatrix * viewMatrix * modelMatrix * vec4(position, 1.0);
 
     vec4 modelPosition = modelMatrix * vec4(position, 1.0);
-    modelPosition.z += sin(modelPosition.x * uFrequency.x - uTime) * 0.1;
-    modelPosition.z += sin(modelPosition.y * uFrequency.y - uTime) * 0.1;
+
+    float elevation = sin(modelPosition.x * uFrequency.x - uTime) * 0.1;
+    elevation += sin(modelPosition.y * uFrequency.y - uTime) * 0.1;
+
+    modelPosition.z += elevation;
     
     vec4 viewPosition = viewMatrix * modelPosition;
     vec4 projectedPosition = projectionMatrix * viewPosition;
     gl_Position = projectedPosition;
 
-    // vRandom = aRandom;
     vUv = uv;
+    vElevation = elevation;
 }
